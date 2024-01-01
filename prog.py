cb5b82d3b1e054051ef41a8b303b62fd3ba641a2
Тимофей <tmsosin@gmail.com>
2024-01-01 16:12:59 +0300

    some changes

--- prog.py
+++ prog.py
@@ -1,7 +1,34 @@
+import pygame
+import sys
+
+
 def main():
-    print('My first git program')
-    print('And I change it every day')
+    pygame.init()
+    pygame.display.set_caption('Шахматная клетка')
+    try:
+        w = int(input())
+        n = int(input())
+    except ValueError:
+        print("Неправильный формат ввода")
+        return -1
+    screen = pygame.display.set_mode((n * 2 * w, n * 2 *w))
+    draw(screen, w, n)
+    while pygame.event.wait().type != pygame.QUIT:
+        pygame.display.flip()
+    pygame.quit()
+
+
+def draw(screen, w, n):
+    color = 'red'
+    for i in range(w * n, w, -w):
+        pygame.draw.circle(screen, color, (n * w, n * w), i)
+        if color == 'red':
+            color = 'blue'
+        elif color == 'blue':
+            color = 'green'
+        else:
+            color = 'red'
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
